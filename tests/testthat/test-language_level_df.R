ddb21a16c31edfd86108667633b4701a9ca8ac56
HedvigS <hedvig.skirgard@gmail.com>
2023-08-31 17:08:23 +0200

    Update test-language_level_df.R

--- tests/testthat/test-language_level_df.R
+++ tests/testthat/test-language_level_df.R
@@ -1,13 +1,10 @@
 test_that("langage_level_df works as expected", {
-    library(reshape2)
     library(dplyr)
     library(readr)
-    library(tidyr)
 
     outcome <- read_csv("fixtures/values.csv", show_col_types = F) %>%
-        make_ValueTable_wide()  %>%
-        language_level_df(method = "singular_least_missing_data", ) %>% nrow()
-
+        language_level_df(method = "singular_least_missing_data", ) %>%        distinct(Language_ID) %>%
+        nrow()
 
     expected <- 6
     expect_equal(outcome, expected)
