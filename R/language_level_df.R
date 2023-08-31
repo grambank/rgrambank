64765716e0bfb1ccad61f62a27c7377af9efa0e3
HedvigS <hedvig.skirgard@gmail.com>
2023-08-31 12:19:43 +0200

    Update language_level_df.R

--- R/language_level_df.R
+++ R/language_level_df.R
@@ -1,54 +1,54 @@
 #' Reduce dialects in dataframe to one per language, and give it the glottocode of the parent that is at level language. Requires a cldf-value table and language-table.
 #'
-#' @param wide_value_table data-frame with wide data from cldf value-table. One column needs to be "Language_ID" and all other columns need to be the relevant variables. There should not be columns with Language meta-data like Longitude, Family_name etc. The data-frame needs to be wide, use function make_ValueTable_wide if need be. The function currently does not support data-sets where Language_ID is not glottocodes (e.g. WALS).
+#' @param ValueTable data-frame with wide data from cldf value-table. One column needs to be "Language_ID" and all other columns need to be the relevant variables. There should not be columns with Language meta-data like Longitude, Family_name etc. The data-frame needs to be wide, use function make_ValueTable_wide if need be. The function currently does not support data-sets where Language_ID is not glottocodes (e.g. WALS).
 #' @param method combine_random = combine all datapoints for all the dialects and if there is more than one datapoint for a given feature/word/variable choose randomly between them, singular_random = choose randomly between the dialects, singular_least_missing_data = choose the dialect which has the most datapoints
-#' @param language_table a filepath to a csv-sheet of a table with glottocodes and language-level glottocodes of the type LanguageTable in the cldf-ontology. if nothing is specified, it fetches table from glottolog-cldf online on GitHub (v4.8).
+#' @param LanguageTable a filepath to a csv-sheet of a table with glottocodes and language-level glottocodes of the type LanguageTable in the cldf-ontology. if nothing is specified, it fetches table from glottolog-cldf online on GitHub (v4.8).
 #' #' @param drop_question_marks If true, question marks are converted to missing values.
 #' @return language-leveled dataset
 
 
-language_level_df <- function(wide_value_table = NULL,
+language_level_df <- function(ValueTable = NULL,
                               method = c( "singular_least_missing_data", "combine_random", "singular_random"),
                               drop_question_marks = FALSE,
-                              language_table_fn = "https://github.com/glottolog/glottolog-cldf/raw/v4.8/cldf/languages.csv"){
+                              LanguageTable_fn = "https://github.com/glottolog/glottolog-cldf/raw/v4.8/cldf/languages.csv"){
 
 ### WRANGLING LANGUAGE TABLE
-#  language_table_fn = "https://raw.githubusercontent.com/glottolog/glottolog-cldf/master/cldf/languages.csv"
-# language_table_fn = "../../../grambank/grambank/cldf/languages.csv"
-# wide_value_table = test_data
+#  LanguageTable_fn = "https://raw.githubusercontent.com/glottolog/glottolog-cldf/master/cldf/languages.csv"
+# LanguageTable_fn = "../../../grambank/grambank/cldf/languages.csv"
+# ValueTable = read_csv("https://github.com/grambank/grambank/raw/master/cldf/values.csv")
 
-language_table <- read_csv(language_table_fn, show_col_types = FALSE)
+LanguageTable <- read_csv(LanguageTable_fn, show_col_types = FALSE)
 
 # The language level ID column is named different in different, CLDF datasets.
 # setting them to the same
-if("Language_ID" %in% colnames(language_table)){
-  language_table   <- language_table %>%
-    dplyr::select(Language_ID = ID, Language_level_ID = Language_ID)
+if(!("Language_level_ID" %in% colnames(LanguageTable))){
+  LanguageTable   <- LanguageTable %>%
+    dplyr::select(Language_ID = Glottocode, Language_level_ID = Language_ID)
 }
 
-if("Language_level_ID" %in% colnames(language_table) & "ID" %in% colnames(language_table)){
-  language_table   <- language_table %>%
-  dplyr::select(Language_ID = ID, Language_level_ID)
+if("Language_level_ID" %in% colnames(LanguageTable) & "ID" %in% colnames(LanguageTable)){
+  LanguageTable   <- LanguageTable %>%
+  dplyr::select(Language_ID = Glottocode, Language_level_ID)
 }
 
 # if there is a missing language level ID, which it can be in some datasets where only dialects get language level IDs and languages and families don't, then replace those with the content in the Language_ID column.
-language_table   <- language_table %>%
+LanguageTable   <- LanguageTable %>%
   mutate(Language_level_ID = ifelse(is.na(Language_level_ID), Language_ID, Language_level_ID))
 
 ## QUESTION MARK ACTION
 
 #drop_question_marks", "keep_question_mark
 if(drop_question_marks == T){
-  wide_value_table <- wide_value_table[wide_value_table == "?"] <- NA
-
+  ValueTable <- ValueTable %>%
+      mutate(Value = ifelse(Value == "?", NA, Value))
 }
 
 ## MERGE FOR LEAST MISSING DATA
 if( method == "singular_least_missing_data"){
-  wide_value_table$na_prop <- apply(dplyr::select(wide_value_table, -Language_ID), 1, function(x) mean(is.na(x)))
+  ValueTable$na_prop <- apply(dplyr::select(ValueTable, -Language_ID), 1, function(x) mean(is.na(x)))
 
-levelled_wide_value_table <- wide_value_table %>%
-  left_join(language_table, by = "Language_ID") %>%
+levelled_ValueTable <- ValueTable %>%
+  left_join(LanguageTable, by = "Language_ID") %>%
   arrange(na_prop) %>%
    distinct(Language_level_ID, .keep_all = TRUE) %>%
   mutate(Language_ID = Language_level_ID) %>%
@@ -62,29 +62,29 @@ if( method == "combine_random"){
 #in order to do this, we need to first make it long again.
 
 # making vector of columns to make long
-    cols <- wide_value_table %>%
+    cols <- ValueTable %>%
     dplyr::select(-Language_ID) %>%
     colnames()
 
-wide_value_table_long <- wide_value_table %>%
+ValueTable_long <- ValueTable %>%
     pivot_longer(names_to = "Parameter_ID", values_to = "Value", cols = all_of(cols)) %>%
     filter(!is.na(Value)) %>%
-    left_join(language_table, by = "Language_ID") %>%
+    left_join(LanguageTable, by = "Language_ID") %>%
     group_by(Language_level_ID, Parameter_ID) %>%
     mutate(n = n()) %>%
    ungroup() %>%
   dplyr::select(-Language_ID)
 
 #it's faster if we do slice_sample (choose randomly) only on those that have more than 1 value per language than if we do it on all.
-wide_value_table_long_n_greater_than_1 <- wide_value_table_long %>%
+ValueTable_long_n_greater_than_1 <- ValueTable_long %>%
   filter(n > 1) %>%
   group_by(Language_level_ID, Parameter_ID) %>%
   slice_sample(n = 1) %>%
   ungroup()
 
-levelled_wide_value_table <- wide_value_table_long %>%
+levelled_ValueTable <- ValueTable_long %>%
   filter(n == 1) %>%
-  full_join(wide_value_table_long_n_greater_than_1 , by = c("Parameter_ID", "Value", "Language_level_ID", "n"))  %>%
+  full_join(ValueTable_long_n_greater_than_1 , by = c("Parameter_ID", "Value", "Language_level_ID", "n"))  %>%
   mutate(Language_ID = Language_level_ID) %>%
   dplyr::select(-Language_level_ID, -n)  %>%
   make_ValueTable_wide()
@@ -92,8 +92,8 @@ levelled_wide_value_table <- wide_value_table_long %>%
 
 # MERGE BY PICKING DIALECTS WHOLLY AT RANDOM
 if( method == "singular_random"){
-levelled_wide_value_table <- wide_value_table %>%
-    left_join(language_table, by = "Language_ID") %>%
+levelled_ValueTable <- ValueTable %>%
+    left_join(LanguageTable, by = "Language_ID") %>%
     group_by(Language_level_ID) %>%
     slice_sample(n = 1) %>%
     ungroup() %>%
@@ -101,7 +101,7 @@ levelled_wide_value_table <- wide_value_table %>%
     dplyr::select(-Language_level_ID)
 }
 
-levelled_wide_value_table
+levelled_ValueTable
 }
 
 
