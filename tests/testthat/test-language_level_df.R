1e61f6f196bec265511d86205ae86744807855a3
HedvigS <hedvig.skirgard@gmail.com>
2023-09-01 13:27:21 +0200

    Update test-language_level_df.R

--- tests/testthat/test-language_level_df.R
+++ tests/testthat/test-language_level_df.R
@@ -1,11 +1,9 @@
 test_that("langage_level_df works as expected", {
-#    library(dplyr)
-#    library(readr)
 
-    outcome <- read_csv("fixtures/testdata/values.csv", show_col_types = F) %>%
-        language_level_df(method = "singular_least_missing_data", ) %>%
+    outcome <- read.delim("fixtures/testdata/values.csv", sep = ",") %>%
+        language_level_df(method = "singular_least_missing_data") %>%
         nrow()
 
-    expected <- 751
+    expected <- 404
     expect_equal(outcome, expected)
 })
