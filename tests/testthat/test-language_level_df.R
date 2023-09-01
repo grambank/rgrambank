153dc49dee0c6f934a0b45caefb96f1d603535f4
HedvigS <hedvig.skirgard@gmail.com>
2023-09-01 14:08:21 +0200

    Update test-language_level_df.R

--- tests/testthat/test-language_level_df.R
+++ tests/testthat/test-language_level_df.R
@@ -4,6 +4,6 @@ test_that("langage_level_df works as expected", {
         language_level_df(method = "singular_least_missing_data") %>%
         nrow()
 
-    expected <- 404
+    expected <- 751
     expect_equal(outcome, expected)
 })
