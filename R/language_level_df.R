a89262fea85eeb1835d0e39a197203a3531b27b9
HedvigS <hedvig.skirgard@gmail.com>
2023-09-01 13:27:19 +0200

    Update language_level_df.R

--- R/language_level_df.R
+++ R/language_level_df.R
@@ -31,7 +31,7 @@ language_level_df <- function(ValueTable = NULL,
     }
 
     ### WRANGLING LANGUAGE TABLE
-    # ValueTable = read_csv("tests/testthat/fixtures/testdata/values.csv")
+    # ValueTable = readr::read_csv("tests/testthat/fixtures/testdata/values.csv")
 
     # TODO SJG: should add some checks here to make sure we have ALL the columns we need
     #  or we stop()
@@ -41,7 +41,7 @@ language_level_df <- function(ValueTable = NULL,
         stop("Invalid table format - ValueTable needs to have columns Language_ID/Parameter_ID/Value")
     }
 
-LanguageTable <- read_csv(LanguageTable_fn, show_col_types = F)
+LanguageTable <- read.delim(LanguageTable_fn, sep = ",")
 
 # The language level ID column is named different in different, CLDF datasets.
 # setting them to the same
@@ -57,32 +57,32 @@ if("Language_level_ID" %in% colnames(LanguageTable) & "ID" %in% colnames(Languag
 
 # if there is a missing language level ID, which it can be in some datasets where only dialects get language level IDs and languages and families don't, then replace those with the content in the Language_ID column.
 LanguageTable   <- LanguageTable %>%
-  mutate(Language_level_ID = ifelse(is.na(Language_level_ID), Language_ID, Language_level_ID))
+  dplyr::mutate(Language_level_ID = ifelse(is.na(Language_level_ID), Language_ID, Language_level_ID))
 
 ## QUESTION MARK ACTION
 
 #turn question marks into missing values if set to TRUE
 if(drop_question_marks == T){
     ValueTable <- ValueTable %>%
-        mutate(Value = ifelse(Value == "?", NA, Value)) %>%
-        filter(!is.na(Value))
+        dplyr::mutate(Value = ifelse(Value == "?", NA, Value)) %>%
+        dplyr::filter(!is.na(Value))
 }
 
 ## MERGE FOR LEAST MISSING DATA
 if( method == "singular_least_missing_data"){
 
     levelled_ValueTable <- ValueTable %>%
-        left_join(LanguageTable, by = "Language_ID") %>%
-        group_by(Language_ID) %>%
-        mutate(n = n()) %>%
-        arrange(desc(n)) %>%
-        ungroup() %>%
-        distinct(Language_level_ID, .keep_all = T) %>%
-        distinct(Language_ID) %>%
-        inner_join(ValueTable, by = "Language_ID") %>%
-        left_join(LanguageTable, by = "Language_ID") %>%
+        dplyr::left_join(LanguageTable, by = "Language_ID") %>%
+        dplyr::group_by(Language_ID) %>%
+        dplyr::mutate(n = dplyr::n()) %>%
+        dplyr::arrange(desc(n)) %>%
+        dplyr::ungroup() %>%
+        dplyr::distinct(Language_level_ID, .keep_all = T) %>%
+        dplyr::distinct(Language_ID) %>%
+        dplyr::inner_join(ValueTable, by = "Language_ID") %>%
+        dplyr::left_join(LanguageTable, by = "Language_ID") %>%
         dplyr::select(-Language_ID) %>%
-        rename(Language_ID = Language_level_ID)
+        dplyr::rename(Language_ID = Language_level_ID)
 
 }
 
@@ -95,24 +95,24 @@ if( method == "combine_random"){
     # making vector of columns to make long
 
     ValueTable_grouped <- ValueTable %>%
-        filter(!is.na(Value)) %>%
-        left_join(LanguageTable, by = "Language_ID",
+        dplyr::filter(!is.na(Value)) %>%
+        dplyr::left_join(LanguageTable, by = "Language_ID",
                   relationship = "many-to-many") %>%
-        group_by(Language_level_ID, Parameter_ID) %>%
-        mutate(n = n()) %>%
-        ungroup() %>%
+        dplyr::group_by(Language_level_ID, Parameter_ID) %>%
+        dplyr::mutate(n = dplyr::n()) %>%
+        dplyr::ungroup() %>%
         dplyr::select(-Language_ID)
 
     #it's faster if we do slice_sample (choose randomly) only on those that have more than 1 value per language than if we do it on all.
     ValueTable_long_n_greater_than_1 <- ValueTable_grouped %>%
-        filter(n > 1) %>%
-        group_by(Language_level_ID, Parameter_ID) %>%
-        slice_sample(n = 1) %>%
-        ungroup()
+        dplyr::filter(n > 1) %>%
+        dplyr::group_by(Language_level_ID, Parameter_ID) %>%
+        dplyr::slice_sample(n = 1) %>%
+        dplyr::ungroup()
 
     levelled_ValueTable <- ValueTable_grouped %>%
-        filter(n == 1) %>%
-        full_join(ValueTable_long_n_greater_than_1, by = join_by(ID,
+        dplyr::filter(n == 1) %>%
+        dplyr::full_join(ValueTable_long_n_greater_than_1, by = join_by(ID,
                                                                  Parameter_ID,
                                                                  Value,
                                                                  Code_ID,
@@ -122,18 +122,18 @@ if( method == "combine_random"){
                                                                  Coders,
                                                                  Language_level_ID,
                                                                  n))  %>%
-        mutate(Language_ID = Language_level_ID) %>%
+        dplyr::mutate(Language_ID = Language_level_ID) %>%
         dplyr::select(-Language_level_ID, -n)
 }
 
 # MERGE BY PICKING DIALECTS WHOLLY AT RANDOM
 if( method == "singular_random"){
     levelled_ValueTable <- ValueTable %>%
-        left_join(LanguageTable, by = "Language_ID") %>%
-        group_by(Language_level_ID) %>%
-        slice_sample(n = 1) %>%
-        ungroup() %>%
-        mutate(Language_ID = Language_level_ID) %>%
+        dplyr::left_join(LanguageTable, by = "Language_ID") %>%
+        dplyr::group_by(Language_level_ID) %>%
+        dplyr::slice_sample(n = 1) %>%
+        dplyr::ungroup() %>%
+        dplyr::mutate(Language_ID = Language_level_ID) %>%
         dplyr::select(-Language_level_ID)
 }
 
