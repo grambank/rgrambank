499d77bbc7aec2ac36848d26e59f31effd931274
SimonGreenhill <SimonGreenhill@users.noreply.github.com>
2023-09-01 13:58:33 +1200

    add namespaces

--- R/binarise.R
+++ R/binarise.R
@@ -21,8 +21,8 @@ binarise <- function(ValueTable = NULL, wide = TRUE, drop_multistate = TRUE, kee
     # if there are binary features coded directly by coders, i.e. not derived from multistate features,
     # we call these "raw binary". users can choose to drop these and only used derived, or use a mix,
     if (keep_raw_binary == FALSE) {
-        ValueTable <-   ValueTable %>%
-            anti_join(dplyr::select(binary_parameters_df, Parameter_ID = ID), by = join_by(Parameter_ID))
+        ValueTable <- ValueTable %>%
+            dplyr::anti_join(dplyr::select(binary_parameters_df, Parameter_ID = ID), by = plyr::join_by(Parameter_ID))
     }
 
     # making the long table wide, it's easier for the binarisation. The table can be made long again with 
@@ -40,13 +40,15 @@ binarise <- function(ValueTable = NULL, wide = TRUE, drop_multistate = TRUE, kee
     wide_values$GB024a <- binarise_gb024_to_gb024a(wide_values$GB024)
     wide_values$GB024b <- binarise_gb024_to_gb024b(wide_values$GB024)
 
+    # are we deliberately using dplyr::if_else AND base::ifelse? 
+
     #GB025 multistate 1: Dem-N; 2: N-Dem; 3: both.
     if("GB025" %in% colnames(wide_values)){
-      wide_values$GB025a <- if_else(wide_values$GB025 == "1"|
+      wide_values$GB025a <- dplyr::if_else(wide_values$GB025 == "1"|
                                       wide_values$GB025 == "3"&
                                       is.na(wide_values$GB025a), "1", ifelse(wide_values$GB025 == "2", "0", wide_values$GB025a))
 
-      wide_values$GB025a <- if_else(wide_values$GB025 == "?"&
+      wide_values$GB025a <- dplyr::if_else(wide_values$GB025 == "?"&
                                       is.na(wide_values$GB025a), "?", wide_values$GB025a)
 
 
@@ -54,81 +56,81 @@ binarise <- function(ValueTable = NULL, wide = TRUE, drop_multistate = TRUE, kee
                                      wide_values$GB025 == "3"&
                                      is.na(wide_values$GB025b), "1", ifelse(wide_values$GB025 == "1", "0", wide_values$GB025b))
 
-      wide_values$GB025b <- if_else(wide_values$GB025 == "?"&
+      wide_values$GB025b <- dplyr::if_else(wide_values$GB025 == "?"&
                                       is.na(wide_values$GB025b), "?", wide_values$GB025b)
 
     }
 
     #GB065 multistate 1:Possessor-Possessed; 2:Possessed-Possessor; 3: both
     if("GB065" %in% colnames(wide_values)){
-      wide_values$GB065a <- if_else(wide_values$GB065 == "1"|
+      wide_values$GB065a <- dplyr::if_else(wide_values$GB065 == "1"|
                                       wide_values$GB065 == "3"&
                                       is.na(wide_values$GB065a), "1", ifelse(wide_values$GB065 == "2", "0", wide_values$GB065a))
 
-      wide_values$GB065a <- if_else(wide_values$GB065 == "?"&
+      wide_values$GB065a <- dplyr::if_else(wide_values$GB065 == "?"&
                                       is.na(wide_values$GB065a), "?", wide_values$GB065a)
 
 
-      wide_values$GB065b <- if_else(wide_values$GB065 == "2"|
+      wide_values$GB065b <- dplyr::if_else(wide_values$GB065 == "2"|
                                       wide_values$GB065 == "3" &
                                       is.na(wide_values$GB065b), "1", ifelse(wide_values$GB065 == "1", "0", wide_values$GB065b))
 
-      wide_values$GB065b <- if_else(wide_values$GB065 == "?" &
+      wide_values$GB065b <- dplyr::if_else(wide_values$GB065 == "?" &
                                       is.na(wide_values$GB065b), "?", wide_values$GB065b)
 
     }
 
     #GB130 multistate 1: SV; 2: VS; 3: both
     if("GB130" %in% colnames(wide_values)){
-      wide_values$GB130a <- if_else(wide_values$GB130 == "1"|
+      wide_values$GB130a <- dplyr::if_else(wide_values$GB130 == "1"|
                                       wide_values$GB130 == "3"&
                                       is.na(wide_values$GB130a), "1", ifelse(wide_values$GB130 == "2", "0", wide_values$GB130a))
 
-      wide_values$GB130a <- if_else(wide_values$GB130 == "?"&
+      wide_values$GB130a <- dplyr::if_else(wide_values$GB130 == "?"&
                                       is.na(wide_values$GB130a), "?", wide_values$GB130a)
 
 
-      wide_values$GB130b <- if_else(wide_values$GB130 == "2"|
+      wide_values$GB130b <- dplyr::if_else(wide_values$GB130 == "2"|
                                       wide_values$GB130 == "3" &
                                       is.na(wide_values$GB130b), "1", ifelse(wide_values$GB130 == "1", "0", wide_values$GB130b))
 
-      wide_values$GB130b <- if_else(wide_values$GB130 == "?"&
+      wide_values$GB130b <- dplyr::if_else(wide_values$GB130 == "?"&
                                       is.na(wide_values$GB130b), "?", wide_values$GB130b)
 
     }
 
     #GB193 multistate 0: they cannot be used attributively, 1: ANM-N; 2: N-ANM; 3: both.
     if("GB193" %in% colnames(wide_values)){
-      wide_values$GB193a <- if_else(wide_values$GB193 == "1"|
+      wide_values$GB193a <- dplyr::if_else(wide_values$GB193 == "1"|
                                       wide_values$GB193 == "3"&
                                       is.na(wide_values$GB193a), "1", ifelse(wide_values$GB193 == "2"|wide_values$GB193 == "0", "0", wide_values$GB193a))
 
-      wide_values$GB193a <- if_else(wide_values$GB193 == "?"&
+      wide_values$GB193a <- dplyr::if_else(wide_values$GB193 == "?"&
                                       is.na(wide_values$GB193a), "?", wide_values$GB193a)
 
-      wide_values$GB193b <- if_else(wide_values$GB193 == "2"|
+      wide_values$GB193b <- dplyr::if_else(wide_values$GB193 == "2"|
                                       wide_values$GB193 == "3"&
                                       is.na(wide_values$GB193b), "1", ifelse(wide_values$GB193 == "1"|wide_values$GB193 == "0", "0", wide_values$GB193b))
 
-      wide_values$GB193b <- if_else(wide_values$GB193 == "?"&
+      wide_values$GB193b <- dplyr::if_else(wide_values$GB193 == "?"&
                                       is.na(wide_values$GB193b), "?", wide_values$GB193b)
 
     }
     #GB203 multistate 0: no UQ, 1: UQ-N; 2: N-UQ; 3: both.
     if("GB203" %in% colnames(wide_values)){
-      wide_values$GB203a <- if_else(wide_values$GB203 == "1"|
+      wide_values$GB203a <- dplyr::if_else(wide_values$GB203 == "1"|
                                       wide_values$GB203 == "3"&
                                       is.na(wide_values$GB203a), "1", ifelse(wide_values$GB203 == "2"|wide_values$GB203 == "0", "0", wide_values$GB203a))
 
-      wide_values$GB203a <- if_else(wide_values$GB203 == "?"&
+      wide_values$GB203a <- dplyr::if_else(wide_values$GB203 == "?"&
                                       is.na(wide_values$GB203a), "?", wide_values$GB203a)
 
 
-      wide_values$GB203b <- if_else(wide_values$GB203 == "2"|
+      wide_values$GB203b <- dplyr::if_else(wide_values$GB203 == "2"|
                                       wide_values$GB203 == "3"&
                                       is.na(wide_values$GB203b), "1", ifelse(wide_values$GB203 == "1"|wide_values$GB203 == "0", "0", wide_values$GB203b))
 
-      wide_values$GB203b <- if_else(wide_values$GB203 == "?"&
+      wide_values$GB203b <- dplyr::if_else(wide_values$GB203 == "?"&
                                       is.na(wide_values$GB203b), "?", wide_values$GB203b)
 
     }
