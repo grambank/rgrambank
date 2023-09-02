04a9782107fc7afbcf33a5e7d34a336ac266dd13
HedvigS <hedvig.skirgard@gmail.com>
2023-09-02 12:00:55 +0200

    rm glottolog 4.8

--- R/drop_duplicate_tips_random.R
+++ R/drop_duplicate_tips_random.R
@@ -1,9 +1,9 @@
 #' If a language tree has tips with duplicate tip labels, drop all but one at random. If there are tips which are dialects of the same language, you can choose to drop all but one.
 #'
-#' @param tree 	an object of class "phylo".
+#' @param tree 	an object of class "phylo" with glottocodes in tip labels.
 #' @param merge_dialects a logical specifying whether to replace dialect tip labels with the glottocode of the language that is their parent, and then drop all but one
 #' @param random_seed integer specifying random seed
-#' @param LanguageTable data-frame of a cldf LanguageTable with columns for glottocodes per languoid and dialects language parent glottocode. If merge_dialects is set to TRUE, a LanguageTable is necessary. If NULL then Glottolog v4.8 is fetched from the internet and used.
+#' @param LanguageTable data-frame of a cldf LanguageTable with columns for glottocodes per languoid and dialects language parent glottocode. If merge_dialects is set to TRUE, a LanguageTable is necessary.
 #' @param trim_tip_label_to_first_eight a logical specifying whether we should trim the tip label to the first 8 characters. If the tip labels contain more than just glottocodes, it is necessary to trim away what is not glottocodes. If set to TRUE, only the first 8 characters are retained. This is necessary for the EDGE-trees as they contain names as well (e.g. "bamu1257_Bamu_Kiwaian"). If the tip labels only consist of glottocodes, this parameter can be set to TRUE or FALSE without difference.
 #' @return tree without duplicates or multiple dialects for the same language.
 #' @export
@@ -11,7 +11,7 @@
 drop_duplicate_tips_random <- function(tree = NULL,
                                       merge_dialects = TRUE,
                                       random_seed = NULL,
-                                      LanguageTable = "Glottolog_4.8",
+                                      LanguageTable = NULL,
                                       trim_tip_label_to_first_eight = TRUE){
 
     #There is a random element in the duplicate dropping. If you want it to give the same result everytime, set a random seed.
@@ -32,11 +32,6 @@ if(merge_dialects == TRUE){
 if(is.null(LanguageTable)){
     stop("LanguageTable is NULL. LanguageTable is necessary for merging dialects.\n")
 }else{
-
-    #unless otherwise specify, use the LanguageTable from glottolog-cldf v4.8 as LanguageTable for matching dialects to their parents.
-    if(LanguageTable == "Glottolog_4.8"){
-        LanguageTable <- read.delim("https://raw.githubusercontent.com/glottolog/glottolog-cldf/v4.8/cldf/languages.csv", sep = ",")
-    }
 
 if(!("Language_ID" %in% colnames(LanguageTable) | "Language_level_ID" %in% colnames(LanguageTable))){
 stop("LanguageTable does not contain 'Language_ID' or 'Language_level_ID' columns.\n")
