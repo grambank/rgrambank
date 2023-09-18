bf04c317019ffd270702bb4632918a2b896cbfd5
HedvigS <hedvig.skirgard@gmail.com>
2023-09-18 21:41:34 +0200

    Update test.add_isolate_info.R

--- tests/test.add_isolate_info.R
+++ tests/test.add_isolate_info.R
@@ -1,8 +1,8 @@
 test_that("add_isolate_info works as expected", {
 
-    outcome <-  read_csv("fixtures/testdata/languages.csv") %>%
+    outcome <-  read_csv("fixtures/testdata/languages.csv", show_col_types = F) %>%
         dplyr::rename(Family_ID = Family_level_ID) %>%
-        add_isolate_info(add_isolate_column = TRUE, set_isolates_family_as = "isolate") %>%
+        add_isolate_info(add_isolate_column = TRUE, set_isolates_Family_ID_as = "missing") %>%
         dplyr::filter(Isolate == "yes") %>%
         nrow()
 
