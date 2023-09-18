e071e546d540f8fe1fd178167b2bc3b1c7a4c4f3
HedvigS <hedvig.skirgard@gmail.com>
2023-09-18 22:39:10 +0200

    Create test-add_family_name_column.R

---
+++ tests/testthat/test-add_family_name_column.R
@@ -0,0 +1,13 @@
+test_that("add_family_name_column works as expected", {
+
+    LanguageTable <- readr::read_csv("fixtures/testdata/languages.csv", show_col_types = F) %>%
+        dplyr::rename(Family_ID = Family_level_ID) %>%
+        dplyr::select(-Family_name)
+    LanguageTable2 <- readr::read_tsv("fixtures/cldf_wide_df_glottolog_4.8.tsv", show_col_types = F)
+
+    outcome <-  add_family_name_column(LanguageTable = LanguageTable, LanguageTable2 = LanguageTable2)
+
+    expectation <-  c("Worrorran", "Worrorran", "Worrorran", "Worrorran", "Worrorran" ,"Worrorran", "Worrorran", "Worrorran")
+
+    expect_equal(outcome$Family_name, expectation)
+})
