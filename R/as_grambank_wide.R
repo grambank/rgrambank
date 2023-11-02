6d6e890f463755a811f8040db614abd34815b6d8
HedvigS <hedvig.skirgard@gmail.com>
2023-11-02 11:53:18 +0100

    Create as_grambank_wide.R

---
+++ R/as_grambank_wide.R
@@ -0,0 +1,20 @@
+#' Extract a wide dataframe from a grambank value table
+#' @param ValueTable a ValueTable dataframe containing the columns Language_ID, Parameter_ID, and Value
+#' @return a wide dataframe.
+#' @export
+#' @examples
+#' gb <- cldf(system.file("tests/testthat/fixtures/testdata/", "StructureDataset-metadata.json", package = "rgrambank"))
+#' values <- as_grambank_wide(gb$tables$ValueTable)
+#' # or:
+#' values <- gb$tables$ValueTable %>% as_grambank_wide()
+as_grambank_wide <- function(ValueTable = NULL) {
+    if (!inherits(ValueTable, "data.frame")) stop("'ValueTable' must be a dataframe.")
+
+    if (!all(c('Language_ID', 'Parameter_ID', 'Value') %in% colnames(ValueTable))) {
+        stop("Invalid table format - ValueTable needs to have columns Language_ID/Parameter_ID/Value")
+    }
+
+    ValueTable %>%
+        dplyr::select(Language_ID, Parameter_ID, Value) %>%
+        tidyr::spread(key = Parameter_ID, value = Value, drop = FALSE)
+}
