035f01dcf148b9c887da8d7ad2bd2e04e2a8418b
HedvigS <hedvig.skirgard@gmail.com>
2023-11-02 11:42:42 +0100

    replace "table" with dplyr::tbl()"

--- R/db.R
+++ R/db.R
@@ -6,17 +6,6 @@ connect <- function(dbfile){
   return(DBI::dbConnect(RSQLite::SQLite(), dbfile))
 }
 
-
-#' Get Grambank table as tibble
-#'
-#' @param conn A database connection as obtained via `connect`
-#' @param name CLDF component name (or CSV filename for custom tables)
-#' @return tibble
-#' @export
-table <- function(conn, name){
-  return(dplyr::tbl(conn, name))
-}
-
 #'
 #'
 #' @param conn A database connection as obtained via `connect`
@@ -24,7 +13,7 @@ table <- function(conn, name){
 #' @param merge_strategy "random_merged"|"random_language"|"best_covered_language"
 #' @export
 merged_values <- function(conn, grouper, merge_strategy){
-  values <- table(conn, 'ValueTable')
+  values <- dplyr::tbl(conn, 'ValueTable')
   #
   # Now join grouper and select according to strategy
   #
